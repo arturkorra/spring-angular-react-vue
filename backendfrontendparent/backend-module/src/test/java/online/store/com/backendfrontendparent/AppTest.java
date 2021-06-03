a0be73eab07ffe3e1a9bbf0a54a4ce253444e79d
arturkorra <arturi21turi@gmail.com>
2021-06-03 12:02:04 +0200

    initial commit

---
+++ backendfrontendparent/backend-module/src/test/java/online/store/com/backendfrontendparent/AppTest.java
@@ -0,0 +1,12 @@
+package online.store.com.backendfrontendparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
